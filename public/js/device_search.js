7823776a2475686e40d9fed50b4a8f331998d535
SamarV-121 <samar@samarv121.dev>
2023-06-02 20:45:59 +0530

    www: Add device search box
    - also switch to flexbox for aside
    
    Change-Id: I08565b9d0d85073b0d978fec4285ae8c5dd639bd

---
+++ public/js/device_search.js
@@ -0,0 +1,19 @@
+const searchDevices = () => {
+  const searchInput = document.getElementById('device-search');
+  const devices = document.getElementsByClassName('device-box');
+  const filter = searchInput.value.trim().toLowerCase();
+
+  Array.from(devices).forEach(device => {
+    const deviceName = device.textContent.trim().toLowerCase();
+    const vendorElement = device.closest('.accordion-item');
+    const accordionInput = vendorElement.querySelector('input[type="radio"]');
+
+    const shouldDisplayDevice = deviceName.includes(filter);
+
+    if (shouldDisplayDevice && filter !== '') {
+      accordionInput.checked = true;
+    } else {
+      accordionInput.checked = false;
+    }
+  });
+};
