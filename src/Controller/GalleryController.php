2efc6e30a43f559c8ea9f2bf2e928e8642d78747
SamarV-121 <samar@samarv121.dev>
2024-04-21 17:47:08 +0530

    www: Update screenshots
    Change-Id: I2ec69ed317977f21346ba8e549e8546fdc8593b8

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -9,11 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class GalleryController extends AbstractController {
     #[Route('/gallery', name: 'leaf_gallery')]
     public function index(): Response {
-        $screenshots = array_filter(scandir($this->getParameter('screenshots_dir')), function ($item) {
-            return !in_array($item, ['.', '..']);
+        $baseDir = $this->getParameter('screenshots_dir');
+        $directories = array_filter(scandir($baseDir), function ($item) {
+            $baseDir = $this->getParameter('screenshots_dir');
+            return is_dir($baseDir . '/' . $item) && !in_array($item, ['.', '..']);
         });
 
-        natsort($screenshots);
+        $screenshots = [];
+
+        foreach ($directories as $directory) {
+            $screenshots[$directory] = array_filter(scandir($baseDir . '/' . $directory), function ($item) {
+                return !in_array($item, ['.', '..']);
+            });
+            natsort($screenshots[$directory]);
+        }
+
         return $this->render(
             'gallery/index.html.twig',
             ['screenshots' => $screenshots]
