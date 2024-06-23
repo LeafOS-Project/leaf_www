889438785c9a0ed8606220de5f06ac4a310776e6
SamarV-121 <samar@samarv121.dev>
2024-06-23 17:52:06 +0530

    www: devices: Improve search bar
    - Properly align it
    - Add support to search using vendor names
    
    Change-Id: I9b2ba34e989178bdc22ce678910718c2ce5b1297

--- public/js/device_search.js
+++ public/js/device_search.js
@@ -1,25 +1,39 @@
 const searchDevices = () => {
-  const searchInput = document.getElementById('device-search');
-  const devices = document.getElementsByClassName('device-box');
+  const searchInput = document.getElementsByClassName('device-search')[0];
   const filter = searchInput.value.trim().toLowerCase();
+  const vendors = document.querySelectorAll('.accordion-item');
 
-  Array.from(devices).forEach(device => {
-    const deviceName = device.textContent.trim().toLowerCase();
-    const vendorElement = device.closest('.accordion-item');
-    const accordionInput = vendorElement.querySelector('input[type="checkbox"]');
+  vendors.forEach(vendorElement => {
+    const devices = vendorElement.querySelectorAll('.device-box');
+    const selectedDevice = vendorElement.querySelector('.selected-device');
+    if (selectedDevice)
+      console.log(selectedDevice.textContent.trim().toLowerCase());
 
-    const shouldDisplayDevice = deviceName.includes(filter);
+    const vendorLabel = vendorElement
+      .querySelector('.accordion-label .label-content span')
+      .textContent.trim()
+      .toLowerCase();
 
-    if (shouldDisplayDevice) {
-      device.style.display = 'block';
-      accordionInput.checked = true;
-    } else {
-      device.style.display = 'none';
-    }
+    devices.forEach(device => {
+      const deviceName = device.textContent.trim().toLowerCase();
+      const accordionInput = vendorElement.querySelector(
+        'input[type="checkbox"]'
+      );
 
-    if (filter === '') {
-      device.style.display = 'block';
-      accordionInput.checked = false;
-    }
+      const shouldDisplayDevice =
+        deviceName.includes(filter) || vendorLabel.includes(filter);
+
+      if (shouldDisplayDevice) {
+        device.style.display = 'block';
+        accordionInput.checked = true;
+      } else {
+        device.style.display = 'none';
+      }
+
+      if (filter === '') {
+        device.style.display = 'block';
+        accordionInput.checked = selectedDevice ? true : false;
+      }
+    });
   });
 };
